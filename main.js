32cda4e778157a433fcb15a491e587c7b18edf92
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-05-06 15:50:28 -0500

    working on module

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-import { getFullName , MAX_FULL_NAME_LENGTH} from './module_one.js';
 
-console.log(getFullName);
-
-
-function test_data_h1(){
-    var test_data = document.getElementById('h1_populate_h1');
-    test_data.innerHTML = MAX_FULL_NAME_LENGTH;
-}
