ada740b18e90d8eec381d0ac336ebe6766c0d07e
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-05-06 15:35:29 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+import { getFullName , test_module} from './module_one.js';
+
+console.log(getFullName);
---
+++ module_one.js
@@ -0,0 +1,8 @@
+export function getFullName() { 
+    return 'test';
+ }
+
+ export var MAX_FULL_NAME_LENGTH = 256;
+
+
+ 
